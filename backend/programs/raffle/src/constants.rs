6a904e59a414d8b7f20a2e82a28c3d0bed725f9a
mbenz12 <mbenz10120112@gmail.com>
2022-07-20 15:14:43 +0800

    code init

---
+++ backend/programs/raffle/src/constants.rs
@@ -0,0 +1,6 @@
+pub const GLOBAL_AUTHORITY_SEED: &str = "global-authority";
+pub const RANDOM_SEED: &str = "random-seed";
+pub const PREY_TOKEN_MINT: &str = "AW2AKm8v7dGvUssdnAJ9Bbbb74t7JQNPDqGzgpDHH3e";
+
+pub const MAX_ENTRANTS: usize = 2000;
+pub const MAX_WINNERS: usize = 50;
---
+++ frontend/src/config.tsx
@@ -0,0 +1,17 @@
+import { PublicKey } from "@solana/web3.js";
+export const NETWORK = "devnet";
+export const ADMINS = [
+  {
+    address: 'M1NXg8Vtwh8mbcY2g8zXJ3drqeueApjmVzHCxV6zy43',
+  },
+];
+
+export const GLOBAL_AUTHORITY_SEED = "global-authority";
+
+export const PROGRAM_ID = "2UJTYXzdemXUydQ9fxYD7ywQXbTGcXZUDcQNRe7khsTQ";
+export const PREY_TOKEN_MINT = new PublicKey("AW2AKm8v7dGvUssdnAJ9Bbbb74t7JQNPDqGzgpDHH3e");
+// export const PREY_TOKEN_MINT = new PublicKey("2Dm1zu8ERJGBs3NLXt8s8Vor3YHwJye5E2pYhLiMHU4L");
+// nYDqQVEaQxLYLh8B8oAFXziMT1bcGrAVigZPL1s3dKc
+export const RAFFLE_SIZE = 66544;
+export const DECIMALS = 1000000000;
+export const PREY_DECIMALS = 1000000000;
